cd383796846d05bd452b3a9643119b7ad7bf0870
Temur <temurisrailov1@gmail.com>
2023-03-02 20:15:25 +0500

    installed bootstrap

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Document</title>
+	<link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+	<script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+	
+</body>
+</html>
